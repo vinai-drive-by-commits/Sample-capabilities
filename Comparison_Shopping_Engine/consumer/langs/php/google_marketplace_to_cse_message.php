3d9e99b28444c8421f1991dd4602b501c1aacec4
Saranyan Vigraham <svigraham@paypal.com>
2012-02-17 12:06:07 -0600

    merged changes from doc team to add comments in the code

--- Comparison_Shopping_Engine/consumer/langs/php/google_marketplace_to_cse_message.php
+++ Comparison_Shopping_Engine/consumer/langs/php/google_marketplace_to_cse_message.php
@@ -1,117 +1,185 @@
 <?php
 include_once 'avro.php';
-//Google product search adapter.
-//Doing a simple product search for some product in the US -
-//https://www.googleapis.com/shopping/search/v1/public/products/?key=KEY&q=QUERY&country=US
+
 //
+// This program implements both an Google product search marketplace adapter
+// and an X.commerce capability that publishes the normalized Google products information to the Fabric
+//
+
+// This function gets product information from the Google product search site using the supplied API key and query string
 function getGoogleProducts($apikey, $query) 
 {
+    // Build a URL that retrieves product info from the Google product search site
+	// model URL - https://www.googleapis.com/shopping/search/v1/public/products/?key=KEY&q=QUERY&country=US
 	$url = "https://www.googleapis.com/shopping/search/v1/public/products/?key=".$apikey."&q=".$query."&country=US";
+	
+	// Initialize a cURL session
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$data = json_decode(curl_exec($ch), true);
+	
+	// Set cURL transfer options
+	curl_setopt($ch, CURLOPT_URL, $url); // URL of the target resource. This URL is the host of the Google product search API + a query string
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it directly.
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // stop cURL from verifying the peer's certificate when the https protocol is used
+	
+	// Send a GET HTTP request containing the URL constructed above
+    // If the request is successful, the returned product information is saved in $json.
+	$json = curl_exec($ch);
+	
+	// Close the cURL session
+	curl_close($ch);
+	
+	// Convert the returned product information (which is in JSON format) into an associative array
+	$data = json_decode($json, true);
+	
+	// Return the value for the "items" key in the product info associative array
 	return $data["items"];
-}
+} // end - getGoogleProducts
 
-//function takes the data returned by google shopping search api and
-//converts it into a CSE message
+
+// This function plugs the information for each product returned by the Google product search API into 
+// a ProductDetails structure. Each such ProductDetails structure is placed in an array.
+// Finally, the function builds a CSE message, which consists of the array of ProductDetails structures 
+// followed by a constant that the identifies the type of product feed - either Full or Incremental
 function convertGoogleDataToCSEMessage($items)
 {
-	//create a test message for cse
-	$products = array();
+	// Create an associative array to hold each cse_product_details structure built by the loop that follows 
+	$cse_product_details = array();
+	
+	// For each Google product search item in the array passed in ...
 	foreach ($items as $item) {
-		$product = $item["product"];
-		//add a condition for if inventories exist -> determines sale price
-		//get the first item in the inventory
-		$inventory_item = array_shift($product["inventories"]);
+		// Assign the value of the product key in the Google item to $google_product
+		$google_product = $item["product"];
+		
+		// Add a condition to handle the case in which inventories exist -> determines sale price
+		// Get the first item in the inventory
+		$inventory_item = array_shift($google_product["inventories"]);
 		$price = $inventory_item["price"];
 		$currency = $inventory_item["currency"];
 		$availability = ucfirst($inventory_item["availability"]);
-		//from google, some product availability is returned as unknown
-		//that will make the schema fail.
+		
+		// For some products, Google sets the availability field to "Unknown"
+		// This value causes Avro encoding to fail due to a conflict with the CSE Avro schema
+		// As a workaround, set availablity to InStock in this case.
 		if ($availability == "Unknown") { $availability = "InStock"; }
 		
-		$ind_product = array("sku" => $product["googleId"],
-						"tile" => $product["title"], 
-						"description" => $product["description"],
-						"manufacturer" => $product["author"]["name"], 
-						"MPN" => "NA", 
-						"GTIN" => "NA", 
-						"brand" => $product["brand"], 
-						"category" => "NA", 
-						"images" => array(),//$product["images"], 
-						"link" => $product["link"], 
-						"condition"=>ucfirst($product["condition"]), 
-						"salePrice" => array("amount" => $price, "code" =>$currency), 
-						"originalPrice" => array("amount" => $price, "code" =>$currency), 
-						"availability" => $availability, 
-						"taxRate" =>array("country" => "US", "region" => "TX", "rate"=> 8.5, "taxShipping" => false), 
-						"shipping" => array("country" => "US", "region" => "TX", "service" => "UPS", "price" => array("amount" => 3.99, "code" =>"USD")), 
-						"shippingWeight" => 0.0, 
-						"attributes" => array(), 
-						"variations" => array(), 
-						"offerId" => "NA", 
-						"cpc" => array("amount" => 0.0, "code" =>"USD"));
-		array_push($products,$ind_product);
+		// For some products, Google does not initialized the brand field.
+		// This causes Avro encoding to fail due to a conflict with the CSE Avro schema
+		// As a workaround, if brand is unitialized, set it to the string "Brand unitialized"
+		if (!isset($google_product["brand"])) $google_product["brand"] = "Brand unitialized";
 		
+		// Populate the cse_product_details array with data for the current product
+		$cse_product_detail = array(
+				"sku" => $google_product["googleId"],
+				"title" => $google_product["title"], 
+				"description" => $google_product["description"],
+				"manufacturer" => $google_product["author"]["name"], 
+				"MPN" => "NA", 
+				"GTIN" => "NA", 
+				"brand" => $google_product["brand"],
+				"category" => "NA", 
+				"images" => array(),//$product["images"], 
+				"link" => $google_product["link"], 
+				"condition"=>ucfirst($google_product["condition"]), 
+				"salePrice" => array("amount" => $price, "code" =>$currency), 
+				"originalPrice" => array("amount" => $price, "code" =>$currency), 
+				"availability" => $availability, 
+				"taxRate" =>array("country" => "US", "region" => "TX", "rate"=> 8.5, "taxShipping" => false), 
+				"shipping" => array("country" => "US", "region" => "TX", "service" => "UPS", "price" => array("amount" => 3.99, "code" =>"USD")), 
+				"shippingWeight" => 0.0, 
+				"attributes" => array(), 
+				"variations" => array(), 
+				"offerId" => "NA", 
+				"cpc" => array("amount" => 0.0, "code" =>"USD"));
+		
+		// Push the product info for the current product into the the $cse_product_details array
+		array_push($cse_product_details, $cse_product_detail);
 	}
-	$message = array("products" => $products, "productFeedType" => "Full");
 	
-	//$message = array("products" => array(array("sku" => "123","tile" => "iphone", "description" => "test iphone","manufacturer" => "apple", "MPN" => "123", "GTIN" => "123", "brand" => "apple", "category" => "electronics", "images" => array(), "link" => "", "condition"=>"New", "salePrice" => array("amount" => 500.0, "code" =>"dollars"), "originalPrice" => array("amount" => 500.0, "code" =>"dollars"), "availability" => "InStock", "taxRate" =>array("country" => "US", "region" => "TX", "rate"=> 8.5, "taxShipping" => false), "shipping" => array("country" => "US", "region" => "TX", "service" => "UPS", "price" => array("amount" => 10.0, "code" =>"dollars")), "shippingWeight" => 10.0, "attributes" => array(), "variations" => array(), "offerId" => " ", "cpc" => array("amount" => 500.0, "code" =>"dollars"))), "productFeedType" => "Full");
+	// Build the CSE message. 
+	// This message is a structure containing an array of ProductDetails structures and 
+	// a value that identifies the type of product feed - either Full or Incremental
+	$message = array("products" => $cse_product_details, "productFeedType" => "Full");
+	
+	// Return the CSE message
 	return $message;	
-}
+} // end - convertGoogleDataToCSEMessage
 
+//
+// Main logic
+//
 
-//$content = file_get_contents("https://ocl.xcommercecloud.com/marketplace/profile/delete/1.0.0");
-//this content should be parsed from a hosted url. 
-$content = file_get_contents("http://localhost/fabric_post/contracts/cse.avpr");
-$testGoogleAPIKey = "AIzaSyAwEqH8vZZ_WI45Eo9RjCzdvLrMZMNzMj8";
+$testGoogleAPIKey = "PUT YOUR KEY HERE"; 
 
+// Get a product list from the Google Products server
+$product_array = getGoogleProducts($testGoogleAPIKey, "droid");
+
+// Convert the Google product list to a CSE message
+$message = convertGoogleDataToCSEMessage($product_array);
+echo print_r($message, true);
+
+// Get the CSE Avro schema from a local copy of cse.avpr
+// In reality, this file will be hosted on the OCL (Open Commerce Language) website 
+// -- $content = file_get_contents("https://ocl.xcommercecloud.com/cse/offer/create/1.0.0");
+// $content = file_get_contents("http://localhost/fabric_post/contracts/cse.avpr");
+$content = file_get_contents("http://localhost/web/cse_demo/cse.avpr");
 if ($content !== false) {
-	echo "Successfully parsed schema!\n";	
+	echo "CSE schema successfully read!\n";	
 }
 
+// Parse the CSE Avro schema and place results in an AvroSchema object
 $schema = AvroSchema::parse($content);
-$datum_writer= new AvroIODatumWriter($schema);
+
+// Create an AvroIODataWriter object for the supplied AvroSchema. 
+// An AvroIODataWriter object handles schema-specific writing of data to the encoder and
+// ensures that each datum written is consistent with the writer's schema.
+$datum_writer = new AvroIODatumWriter($schema);
+
+// Create an AvroStringIO object - this is an AvroIO wrapper for string I/O
 $write_io = new AvroStringIO();
+
+// Create an AvroIOBinaryEncoder object.
+// This object encodes and writes Avro data to the supplied AvroIO object using Avro binary encoding.
 $encoder = new AvroIOBinaryEncoder($write_io);
 
-//get the google products list
-$product_array = getGoogleProducts($testGoogleAPIKey, "iphone");
-//convert the google product list to CSE message
-$message = convertGoogleDataToCSEMessage($product_array);
-echo print_r($message,true);
-
-//create a test message for cse
-//$message = array("products" => array(array("sku" => "123","tile" => "iphone", "description" => "test iphone","manufacturer" => "apple", "MPN" => "123", "GTIN" => "123", "brand" => "apple", "category" => "electronics", "images" => array(), "link" => "", "condition"=>"New", "salePrice" => array("amount" => 500.0, "code" =>"dollars"), "originalPrice" => array("amount" => 500.0, "code" =>"dollars"), "availability" => "InStock", "taxRate" =>array("country" => "US", "region" => "TX", "rate"=> 8.5, "taxShipping" => false), "shipping" => array("country" => "US", "region" => "TX", "service" => "UPS", "price" => array("amount" => 10.0, "code" =>"dollars")), "shippingWeight" => 10.0, "attributes" => array(), "variations" => array(), "offerId" => " ", "cpc" => array("amount" => 500.0, "code" =>"dollars"))), "productFeedType" => "Full");
 try {
-	$datum_writer->write($message,$encoder);
-	//send the test message to the local fabric
+		// Binary-encode and serialize the supplied CSE message using the CSE schema and the supplied encoder
+		// The result is stored in the AvroStringIO object $write_io created above
+		$datum_writer->write($message, $encoder);
+	}
+	catch (Exception $e) {
+		echo "Message does not adhere to schema!";
+		echo "Exception object:" . $e;
+	} // end - try block
+	
+	//
+	// Send the CSE message to the Fabric on the topic /cse/offer/create
+	//
+	
+try {
+	// Initialize a cURL session
 	$ch = curl_init();
 	
-	// The URL is the host:port for the XFabric, plus the topic
-	curl_setopt($ch, CURLOPT_URL, "https://localhost:8080/cse/offers/create");
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, true);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	//curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: avro/binary", "Authorization: Bearer QUkAAV4C7dkO5NH/c1IGdFeZeaqMbeiZe1yoGp9QHxVrXpMFwdBc0hfiBE+uNVI3nT1vlA==","X-XC-SCHEMA-VERSION: 1.0.0", "X-XC-SCHEMA-URL: http://localhost/fabric_post/contracts/cse.avpr"));
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: avro/binary", "Authorization: Bearer QUkAAZORF/BpTsy9+pKy6iFIjPu1Wc+/XKDZQCjve56KgXxpXvdIzTHykSlSbC4bdwsQ1A==","X-XC-SCHEMA-VERSION: 1.0.0", "X-XC-SCHEMA-URI: http://localhost/fabric_post/contracts/cse.avpr"));
+	// Set the cURL options for this session
+	curl_setopt($ch, CURLOPT_URL, "https://localhost:8080/cse/offer/create"); // URL of the target resource. This URL is the host:port of the Fabric, with the topic appended
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // stop cURL from verifying the peer's certificate when the https protocol is used
+	curl_setopt($ch, CURLOPT_HEADER, true); // TRUE to include the header in the output
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10); // maximum number of seconds to allow cURL functions to execute
+	curl_setopt($ch, CURLOPT_POST, true); // TRUE to do a regular HTTP POST. This POST is the normal application/x-www-form-urlencoded kind, most commonly used by HTML forms. 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: avro/binary", "Authorization: Bearer QUkAAaM+u42KAGU0d8kb819B9LUtB7G5IWLz//45TKM9au9xlWaen5ZHH1yn5OqlPk+HRQ==","X-XC-SCHEMA-VERSION: 1.0.0", "X-XC-SCHEMA-URI: http://localhost/web/cse_demo/cse.avpr")); // An array of HTTP header fields to set, in the format array('Content-type: text/plain', 'Content-length: 100')
 	
+	// Add the binary-encoded, serialized CSE message to the HTTP message as the message body
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $write_io->string()); // The full data to post in an HTTP "POST" operation.
 
-	// Send the message
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $write_io->string());
-
+	// POST the HTTP request to the Fabric and print the response returned by the Fabric
 	$response = curl_exec($ch);
 	print $response;
+	}
+	catch (Exception $e) {
+		echo "Error POSTing message to Fabric!";
+		echo "Exception object:" . $e;
+	} // end - try block
 	
-} catch (Exception $e) {
-	echo "Message does not adhere to schema!";
-}
-
-
-
+// end - cse_publisher.php
 
 ?>
--- Comparison_Shopping_Engine/provider/langs/php/index.php
+++ Comparison_Shopping_Engine/provider/langs/php/index.php
@@ -3,11 +3,9 @@
 require __DIR__.'/lib/base.php';
 include_once 'avro.php';
 
-
-
 F3::route('GET /','home');
 	function home() {
-		echo "Hello, X.commerce! You should be posting to /cse/offers/create\n";
+		echo "Hello, X.commerce! You should be posting to /cse/offer/create\n";
 		
 	}
 	
@@ -19,46 +17,80 @@ F3::route('GET /testphp' ,
 	}
 );
 	
-F3::route('POST /cse/offers/create','cse');
-	function cse() {
-		//writing a log file to dump our outputs
+F3::route('POST /cse/offer/create','cse');
+function cse() {
+		// Open the log file to which to write outputs
 		$fp = fopen('test.log', 'at');
 		
+		// Get all http headers in the received message
 		$headers = getallheaders();
-		//get it from X-XC-SCHEMA-URI, the default location
-                $content = file_get_contents($headers['X-XC-SCHEMA-URI']);		
+		
+        // Get the posted message body
+        // NOTE: The message body is currently in Avro binary form
 		$post_data = file_get_contents("php://input");
-		$schema = AvroSchema::parse($content);
+		
+		// Get the URI of the Avro schema on the OCL server that adheres to the /cse/offer/create contract 
+		$schema_uri = $headers['X-XC-SCHEMA-URI'];
+		
+		// Get the contents of the Avro schema identified by the URI retrieved above
+        $content = file_get_contents($schema_uri);
+
+		// Parse the CSE Avro schema and place results in an AvroSchema object
+        $schema = AvroSchema::parse($content);
+		
 		//fwrite($fp, $schema);
 		//fwrite($fp, "\n");
+		
+		// Use Avro to decode and deserialize the binary-encoded message body.
+		// The result is the plain text version of the message body
+		// The message sender used Avro to binary-encode the text version of the message body before sending the message.
+		
+		// Create an AvroIODatumReader object for the supplied AvroSchema.
+		// An AvroIODatumReader object handles schema-specific reading of data from the decoder and
+		// ensures that each datum read is consistent with the reader's schema.		
 		$datum_reader = new AvroIODatumReader($schema);
+		
+		// Create an AvroStringIO object and assign it the encoded message body
 		$read_io = new AvroStringIO($post_data);
+		
+		// Create an AvroIOBinaryDecoder object and assign it the $read_io object
 		$decoder = new AvroIOBinaryDecoder($read_io);
+		
+		// Decode and deserialize the data using the CSE schema and the supplied decoder
+		// The data is retrieved from the AvroStringIO object $read_io created above
+		// Upon return, $message contains the plain text version of the X.commerce message sent by the publisher
 		$message = $datum_reader->read($decoder);
+		
 		//fwrite($fp, $post_data);
 		fwrite($fp, "\n");
 		//fwrite($fp, print_r($message, true));
 		fwrite($fp, print_r($headers,true));
 		
-		
-		//write to mongodb
+		// Connect to the Mongo server running on your machine
+		// NOTE: you must start the Mongo server prior to running this web application 
 		$conn = new Mongo('localhost');
-		// access database
-	    $db = $conn->cse_data;
-	    // access collection
-	    $collection = $db->google;
-	    // insert a new document
-	   $item = $message["products"];
-	   $collection->insert($item);
-	
-		//write to log file for our sake
-	   fwrite($fp, print_r($item, true));
-	   fwrite($fp,"Inserted document with ID: " . $item['_id']);
-	   fclose($fp);
-	   // disconnect from server
-	   $conn->close();
 		
+		// Access the cse_data database 
+		// If this database does not exist, Mongo creates it
+	    $db = $conn->cse_data;
+	    
+	    // Access the google collection
+	    // If this collection does not exist, Mongo creates it
+	    $collection = $db->google;
+	    
+	    // Insert a new document into the google collection
+	    $item = $message["products"];
+	    $collection->insert($item);
+	
+		// Write to log file
+	    fwrite($fp, print_r($item, true));
+	    fwrite($fp,"Inserted document with ID: " . $item['_id']);
+	    fclose($fp);
+	   
+	    // Disconnect from the MongoDB server
+	    $conn->close();
 	}
+	
 F3::run();
 
 ?>
