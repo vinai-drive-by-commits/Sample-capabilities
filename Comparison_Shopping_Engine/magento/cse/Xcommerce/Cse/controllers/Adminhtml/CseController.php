45ee6df16fccd267aa127396957822152eea5ff1
Vinai Kopp <vinai@netzarbeiter.com>
2012-03-16 19:54:04 +0100

    Move controller into admin area

---
+++ Comparison_Shopping_Engine/magento/cse/Xcommerce/Cse/controllers/Adminhtml/CseController.php
@@ -0,0 +1,12 @@
+<?php
+
+class Xcommerce_Cse_Adminhtml_CseController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+	{
+		$this->loadLayout()
+			->_addContent($this->getLayout()->createBlock('cse/listProducts'))
+			->renderLayout();
+	}
+	
+}
