422d5357bed1fd4286ac835bfa495eb7018b8c5a
Vinai Kopp <vinai@netzarbeiter.com>
2012-03-16 20:02:34 +0100

    Remove confusing comment that would make sense in a block class

--- Comparison_Shopping_Engine/magento/cse/Xcommerce/Cse/controllers/IndexController.php
+++ Comparison_Shopping_Engine/magento/cse/Xcommerce/Cse/controllers/IndexController.php
@@ -1,22 +1,15 @@
 <?php
 class Xcommerce_Cse_IndexController extends Mage_Core_Controller_Front_Action
 {
- 
-    public function indexAction()
-           {
-       	$this->loadLayout();
-       	$this->getLayout()
-       		->getBlock('content')->append(
-       			$this->getLayout()->createBlock('cse/helloWorld')
-           	);
-       	
-               $this->renderLayout();
-           }
-	// public function __construct()
-	// 	    {
-	// 	        parent::__construct();
-	// 	        $this->setTemplate('cse/list_products.phtml');
-	// 	    }
 
-	   
+	public function indexAction()
+	{
+		$this->loadLayout();
+		$this->getLayout()
+			->getBlock('content')->append(
+			$this->getLayout()->createBlock('cse/helloWorld')
+		);
+
+		$this->renderLayout();
+	}
 }
