5923823b739fb7b4119aa359f5ad40a35c4dd800
Vinai Kopp <vinai@netzarbeiter.com>
2012-03-16 19:56:34 +0100

    Check ACL for admin user permissions for cse controller

--- Comparison_Shopping_Engine/magento/cse/Xcommerce/Cse/controllers/Adminhtml/CseController.php
+++ Comparison_Shopping_Engine/magento/cse/Xcommerce/Cse/controllers/Adminhtml/CseController.php
@@ -8,5 +8,9 @@ class Xcommerce_Cse_Adminhtml_CseController extends Mage_Adminhtml_Controller_Ac
 			->_addContent($this->getLayout()->createBlock('cse/listProducts'))
 			->renderLayout();
 	}
-	
+
+	protected function _isAllowed()
+	{
+		return Mage::getSingleton('admin/session')->isAllowed('xcommerce/cse');
+	}
 }
