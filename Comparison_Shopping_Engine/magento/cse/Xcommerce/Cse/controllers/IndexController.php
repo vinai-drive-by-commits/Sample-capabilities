1cbde9d932ab42608f4f3a05418e828b0beba63c
Vinai Kopp <vinai@netzarbeiter.com>
2012-03-16 20:04:31 +0100

    Whitespace changes

--- Comparison_Shopping_Engine/magento/cse/Xcommerce/Cse/controllers/IndexController.php
+++ Comparison_Shopping_Engine/magento/cse/Xcommerce/Cse/controllers/IndexController.php
@@ -1,15 +1,14 @@
 <?php
+
 class Xcommerce_Cse_IndexController extends Mage_Core_Controller_Front_Action
 {
-
 	public function indexAction()
 	{
 		$this->loadLayout();
 		$this->getLayout()
 			->getBlock('content')->append(
-			$this->getLayout()->createBlock('cse/helloWorld')
-		);
-
+				$this->getLayout()->createBlock('cse/helloWorld')
+			);
 		$this->renderLayout();
 	}
 }
